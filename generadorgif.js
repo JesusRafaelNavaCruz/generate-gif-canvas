091473e344cc63173e8cabfd4a9c20c713ec06ca
JesusRafaelNavaCruz <jnava@erillamhc.com>
2024-05-13 17:53:52 -0600

    Create project

---
+++ generadorgif.js
@@ -0,0 +1,89 @@
+const { createCanvas } = require("canvas");
+const GIFEncoder = require("gifencoder");
+
+// Función para generar un fotograma del GIF con el texto del contador regresivo
+function generarFotograma(canvas, context, tiempoRestante) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const expirationDate = new Date(tiempoRestante);
+
+    // Calcula el tiempo restante en milisegundos
+    const now = new Date();
+    const timeLeft = expirationDate.getTime() - now.getTime();
+  
+    // Calcula los días, horas, minutos y segundos restantes
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((timeLeft / (1000 * 60)) % 60);
+    const seconds = Math.floor((timeLeft / 1000) % 60);
+
+    context.fillStyle = "#f4f4f4"
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    
+    const numLabels = 4
+    const widthPercent = 0.9
+    const padding = (canvas.width * (1 - widthPercent)) / 2
+    const anchoEspacio = (canvas.width - (padding * 2)) / (numLabels + 1);
+
+    // Dibujar el texto en el lienzo
+    context.font = "bold 60px Arial";
+    context.fillStyle = "#454545";
+    context.textAlign = "center";
+    context.fillText(`${days}`, 20 + 20 + 20, 100);
+    context.fillText(`${hours}`, 20 + 135 + 20 + 20, 100);
+    context.fillText(`${minutes}`, 20 + 135 + 20 + 135 + 20 + 20, 100);
+    context.fillText(`${seconds}`, 20 + 135 + 20 + 135 + 20 + 135 + 20 + 20, 100);
+
+    // Dibujar labels
+    context.font = "16px Arial";
+    context.fillStyle = "#454545";
+    context.textAlign = "center"
+    context.fillText("Días",  20 + 20 + 20, 150);
+    context.fillText("Horas", 20 + 135 + 20 + 20, 150);
+    context.fillText("Minutos", 20 + 135 + 20 + 135 + 20 + 20, 150);
+    context.fillText("Segundos", 20 + 135 + 20 + 135 + 20 + 135 + 20 + 20, 150);
+
+    return context.getImageData(0, 0, canvas.width, canvas.height).data;
+}
+
+// Función para generar el GIF animado con el contador regresivo
+async function generarGIF(tiempoRestante) {
+    const canvas = createCanvas(600, 200);
+    const context = canvas.getContext("2d");
+    const encoder = new GIFEncoder(600, 200);
+
+    // Configurar el encoder
+    encoder.start();
+    encoder.setRepeat(0); // Repetir indefinidamente
+    encoder.setDelay(1000); // 1 segundo por fotograma
+
+    // Generar cada fotograma del GIF
+    for (let i = 0; i < 120; i++) { // Generar 10 fotogramas (10 segundos)
+        const fotogramaData = generarFotograma(canvas, context, tiempoRestante);
+        encoder.addFrame(context);
+        tiempoRestante.seconds--; // Actualizar el tiempo restante para el siguiente fotograma
+
+        if (tiempoRestante.seconds < 0) {
+            tiempoRestante.seconds = 59;
+            tiempoRestante.minutes--;
+            if (tiempoRestante.minutes < 0) {
+                tiempoRestante.minutes = 59;
+                tiempoRestante.hours--;
+                if (tiempoRestante.hours < 0) {
+                    tiempoRestante.hours = 23;
+                    tiempoRestante.days--;
+                }
+            }
+        }
+        
+        // Esperar 1 segundo antes de generar el siguiente fotograma
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    // Finalizar el encoder y devolver el GIF como un buffer
+    encoder.finish();
+    return encoder.out.getData();
+}
+
+module.exports = { generarGIF };
---
+++ main.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { generarGIF } = require('./generadorgif');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+
+const app = express();
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION
+});
+
+const s3 = new AWS.S3();
+
+app.get("/", async (req, res) => {
+    res.status(200).send("Hola mundo")
+});
+
+// Ruta para generar y servir el GIF animado
+app.get('/generar-gif', async (req, res) => {
+    try {
+        const now = new Date()
+        const target_date_time = req.query.expirationDate || now.setDate(now.getDate() + 1)
+        const nameImage = `${req.query.name}.gif` || "new_image.gif";
+        const gifBuffer = await generarGIF(target_date_time); // Llama a la función para generar el GIF
+
+        const uploadParams = {
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: nameImage,
+            Body: gifBuffer, 
+            ContentType: "image/gif",
+            ACL: "public-read",
+        }
+
+        const uploadResult = await s3.upload(uploadParams).promise();
+
+        const gifUrl = uploadResult.Location;
+        console.log(gifUrl);
+
+        res.set('Content-Type', 'image/gif');
+        // res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+
+        res.json({ gifUrl })
+
+    } catch (error) {
+        console.error('Error al generar el GIF:', error);
+        res.status(500).send('Error interno del servidor');
+    }
+});
+
+// Puerto en el que escucha el servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
